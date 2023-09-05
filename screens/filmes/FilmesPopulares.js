179311868fb97e630ac54e80f22e353417fccf5f
matheus rodrigues monte <matheusr.monte2004@gmail.com>
2023-09-04 22:17:09 -0300

    filmes

---
+++ screens/filmes/FilmesPopulares.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native-paper'
+
+
+export const FilmesPopulares = () => {
+  return (
+    <Text>FilmesPopulares</Text>
+  )
+}
+
+export default FilmesPopulares
---
+++ services/apiFilmes.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+const apiFilmes = axios.create({
+    baseURL:' https://api.themoviedb.org/3/',
+    headers:{
+        Authorization:'Bearer '
+    }
+})
+
+export default apiFilmes
